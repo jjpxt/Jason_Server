e3ed23418521aa3a81e8469099e046bc8b542c9c
jjpxt <jotajunior.peixoto@gmail.com>
2026-02-14 18:42:47 -0300

    json server

--- js/details.js
+++ js/details.js
@@ -1,6 +1,6 @@
-const id = new URLSearchParams(window.location.search).get('id');
-const container = document.querySelector('.details');
 const deleteBtn = document.querySelector('.btn');
+const container = document.querySelector('.details');
+const id = new URLSearchParams(window.location.search).get('id');
 
 const renderDetails = async () => {
     const res = await fetch('http://localhost:3000/posts/' + id);
@@ -17,7 +17,7 @@ deleteBtn.addEventListener('click', async (e) => {
     const res = await fetch('http://localhost:3000/posts/' + id, {
         method: 'DELETE'
     });
-    
+
     window.location.replace('/index.html');
 });
 
--- js/index.js
+++ js/index.js
@@ -1,20 +1,3 @@
-// npx json-server --watch data/db.json
-
-//  Na versão 1.x (beta) do json-server o comportamento mudou de propósito:id sempre é 
-// tratado como  string internamente.
-// Quando você faz um POST sem fornecer um "id" no body, ele gera automaticamente 
-// um ID aleatório curto (tipo 4 caracteres hexadecimais, como "54ae", "c917" etc.).
-// Não existe mais autoincremento numérico (1 → 2 → 3...) nessa versão, mesmo se os
-//  IDs existentes forem números. 
-// É o design novo: "id is always a string and will be generated for you if missing" 
-
-// Downgrade para a versão estável antiga (v0.17.x) — onde autoincremento numérico funciona
-// Isso resolve 100% o que você quer (IDs sequenciais 7, 8, 9...).
-// Passos: 1° npm uninstall json-server
-//             2° npm install json-server@0.17.4
-
-
-
 const container = document.querySelector(".blogs");
 
 const renderPosts = async () => {
@@ -35,7 +18,6 @@ const renderPosts = async () => {
     });
 
     container.innerHTML = template;
-
 }
 
 window.addEventListener("DOMContentLoaded", () => renderPosts());
